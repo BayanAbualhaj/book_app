c03b2c0b3ea13acf5693f34789761a837adc3288
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-03-01 22:46:04 +0200

    =((

--- server.js
+++ server.js
@@ -5,50 +5,48 @@ const cors = require('cors');
 const superAgent = require('superagent');
 const app = express();
 
-
-
 require('dotenv').config();
+
+
 app.use(cors());
-// let client = new pg.Client(process.env.DATABASE_URL);
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static('./public'));
+app.set('view engine', 'ejs');
+
 const PORT = process.env.PORT;
 
+// let client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 
 
 
-app.set('view engine', 'ejs');
 app.get('/', handleHome);
 app.get('/searches/new', handleNew);
 app.post('/searches', handleSearches);
 app.post('/books', handleBooks);
 app.get('/books/:id', handleOneBook);
-// app.post('/books/:id', handleOneBook);
-
-
 app.get('*', handleError);
 
 const googleAPI = 'https://www.googleapis.com/books/v1/volumes';
 
-
+//=====================Handle Home=====================
 
 function handleHome(req, res) {
-    // console.log("Hello");
     client.query('SELECT * FROM books').then(data => {
-        // console.log(data.rows);
-        
+      
         res.render('pages/index', { favBooks: data.rows })
     }).catch(error => {
         res.status(500).render('pages/error');
     });
 }
 
+//==================Handle New search=====================
 
 function handleNew(req, res) {
     res.render('pages/searches/new');
 }
 
+//===================Handle searches=========================
 function handleSearches(req, res) {
     let searchQuery = req.body.searchQuery;
     let searchWith = req.body.searchBy;
@@ -81,10 +79,11 @@ function handleSearches(req, res) {
     });
 }
 
+//===================Handle Books=====================
+
 function handleBooks(req, res) {
 
     let reqBody =req.body;
-    // console.log(req.body);
     let insertQuery= 'INSERT INTO books(author,title,isbn,image_url,description) VALUES ($1,$2,$3,$4,$5) RETURNING *;';
 
     let safeValue= [reqBody.authors,reqBody.title,reqBody.isbn,reqBody.image,reqBody.description];
@@ -99,6 +98,7 @@ function handleBooks(req, res) {
 
 }
 
+//=====================Handle One Book===================
 
 function handleOneBook(req,res){
     // console.log('gfkufh',req.params);
@@ -117,6 +117,8 @@ function handleOneBook(req,res){
 
 }
 
+//====================Book Constructor=======================
+
 function Book(imgURL, title, isbn, authors, description) {
     this.thumbnail = imgURL || 'https://i.imgur.com/J5LVHEL.jpg';
     this.title = title || 'there is no title';
@@ -125,11 +127,14 @@ function Book(imgURL, title, isbn, authors, description) {
     this.description = description || 'No description was found';
 }
 
+//======================Handle Errors=======================
+
 function handleError(req, res) {
     res.render('pages/error');
 }
 
 
+//============Connect to DB & Listener=======================
 
 
 client.connect().then(() => {
